0a761182d7ea9a9e32af09d7ffaa63d55527e846
Cezar Kellermann de Carvalho <cezar_kc@hotmail.com>
2021-07-19 16:54:59 -0300

    inverter mapping profile

--- ContactsDemo.Configuration/MappingProfile.cs
+++ ContactsDemo.Configuration/MappingProfile.cs
@@ -9,23 +9,17 @@ namespace ContactsDemo.Configuration
 
         public MappingProfile()
         {
-            CreateMap<NaturalPersonContactDTO, NaturalPerson>();
-            CreateMap<LegalPersonContactDTO, LegalPerson>();
-            CreateMap<NaturalPersonContactDTO, Address>();
-            CreateMap<LegalPersonContactDTO, Address>();
-
-
             CreateMap<Contact, NaturalPersonContactDTO>()
-                .ForMember(dest => dest.AddressLine1, from => from.MapFrom(x => x.Address.AddressLine1))
-                .ForMember(dest => dest.AddressLine2, from => from.MapFrom(x => x.Address.AddressLine2))
-                .ForMember(dest => dest.City, from => from.MapFrom(x => x.Address.City))
-                .ForMember(dest => dest.Country, from => from.MapFrom(x => x.Address.Country))
-                .ForMember(dest => dest.State, from => from.MapFrom(x => x.Address.State))
-                .ForMember(dest => dest.ZipCode, from => from.MapFrom(x => x.Address.ZipCode))
-                .ForMember(dest => dest.Birthday, from => from.MapFrom(x => x.Person.NaturalPerson.Birthday))
-                .ForMember(dest => dest.Name, from => from.MapFrom(x => x.Person.NaturalPerson.Name))
-                .ForMember(dest => dest.Gender, from => from.MapFrom(x => x.Person.NaturalPerson.Gender))
-                .ForMember(dest => dest.CPF, from => from.MapFrom(x => x.Person.NaturalPerson.CPF));
+               .ForMember(dest => dest.AddressLine1, from => from.MapFrom(x => x.Address.AddressLine1))
+               .ForMember(dest => dest.AddressLine2, from => from.MapFrom(x => x.Address.AddressLine2))
+               .ForMember(dest => dest.City, from => from.MapFrom(x => x.Address.City))
+               .ForMember(dest => dest.Country, from => from.MapFrom(x => x.Address.Country))
+               .ForMember(dest => dest.State, from => from.MapFrom(x => x.Address.State))
+               .ForMember(dest => dest.ZipCode, from => from.MapFrom(x => x.Address.ZipCode))
+               .ForMember(dest => dest.Birthday, from => from.MapFrom(x => x.Person.NaturalPerson.Birthday))
+               .ForMember(dest => dest.Name, from => from.MapFrom(x => x.Person.NaturalPerson.Name))
+               .ForMember(dest => dest.Gender, from => from.MapFrom(x => x.Person.NaturalPerson.Gender))
+               .ForMember(dest => dest.CPF, from => from.MapFrom(x => x.Person.NaturalPerson.CPF));
 
             CreateMap<Contact, LegalPersonContactDTO>()
                 .ForMember(dest => dest.AddressLine1, from => from.MapFrom(x => x.Address.AddressLine1))
@@ -37,6 +31,11 @@ namespace ContactsDemo.Configuration
                 .ForMember(dest => dest.CNPJ, from => from.MapFrom(x => (x.Person as LegalPerson).CNPJ))
                 .ForMember(dest => dest.CompanyName, from => from.MapFrom(x => (x.Person as LegalPerson).CompanyName))
                 .ForMember(dest => dest.TradeName, from => from.MapFrom(x => (x.Person as LegalPerson).TradeName));
+
+            CreateMap<NaturalPersonContactDTO, NaturalPerson>();
+            CreateMap<LegalPersonContactDTO, LegalPerson>();
+            CreateMap<NaturalPersonContactDTO, Address>();
+            CreateMap<LegalPersonContactDTO, Address>();           
         }
 
     }
