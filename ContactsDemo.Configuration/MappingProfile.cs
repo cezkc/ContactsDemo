83af3464230749ef6b730b8197594ff7ddb78692
Cezar Kellermann de Carvalho <cezar_kc@hotmail.com>
2021-07-19 17:23:10 -0300

    change maping feature 321

--- ContactsDemo.Configuration/MappingProfile.cs
+++ ContactsDemo.Configuration/MappingProfile.cs
@@ -9,23 +9,26 @@ namespace ContactsDemo.Configuration
 
         public MappingProfile()
         {
-            
+
+
+            CreateMap<Contact, LegalPersonContactDTO>()
+               .ForMember(dest => dest.AddressLine1, from => from.MapFrom(x => x.Address.AddressLine1))
+               .ForMember(dest => dest.AddressLine2, from => from.MapFrom(x => x.Address.AddressLine2))
+               .ForMember(dest => dest.City, from => from.MapFrom(x => x.Address.City))
+               .ForMember(dest => dest.Country, from => from.MapFrom(x => x.Address.Country))
+               .ForMember(dest => dest.State, from => from.MapFrom(x => x.Address.State))
+               .ForMember(dest => dest.ZipCode, from => from.MapFrom(x => x.Address.ZipCode))
+               .ForMember(dest => dest.CNPJ, from => from.MapFrom(x => (x.Person as LegalPerson).CNPJ))
+               .ForMember(dest => dest.CompanyName, from => from.MapFrom(x => (x.Person as LegalPerson).CompanyName))
+               .ForMember(dest => dest.TradeName, from => from.MapFrom(x => (x.Person as LegalPerson).TradeName));
+
             CreateMap<LegalPersonContactDTO, Address>();
             CreateMap<NaturalPersonContactDTO, Address>();
             CreateMap<NaturalPersonContactDTO, NaturalPerson>();
             CreateMap<LegalPersonContactDTO, LegalPerson>();
 
 
-            CreateMap<Contact, LegalPersonContactDTO>()
-                .ForMember(dest => dest.AddressLine1, from => from.MapFrom(x => x.Address.AddressLine1))
-                .ForMember(dest => dest.AddressLine2, from => from.MapFrom(x => x.Address.AddressLine2))
-                .ForMember(dest => dest.City, from => from.MapFrom(x => x.Address.City))
-                .ForMember(dest => dest.Country, from => from.MapFrom(x => x.Address.Country))
-                .ForMember(dest => dest.State, from => from.MapFrom(x => x.Address.State))
-                .ForMember(dest => dest.ZipCode, from => from.MapFrom(x => x.Address.ZipCode))
-                .ForMember(dest => dest.CNPJ, from => from.MapFrom(x => (x.Person as LegalPerson).CNPJ))
-                .ForMember(dest => dest.CompanyName, from => from.MapFrom(x => (x.Person as LegalPerson).CompanyName))
-                .ForMember(dest => dest.TradeName, from => from.MapFrom(x => (x.Person as LegalPerson).TradeName));
+           
 
             CreateMap<Contact, NaturalPersonContactDTO>()
               .ForMember(dest => dest.AddressLine1, from => from.MapFrom(x => x.Address.AddressLine1))
